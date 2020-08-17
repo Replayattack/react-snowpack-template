b8b3d3a09e0de0ee871c0a70c655a32a47cfdb9f
Daniel G <replayattack@pm.me>
2020-08-16 23:57:07 -0500

    Update Prettier configuration and apply format

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  expect.hasAssertions();
+  expect.hasAssertions()
 
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/learn react/i)
 
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(linkElement).toBeInTheDocument()
+})
