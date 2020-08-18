b4689312b32ba455db944a48e5170750ed4a3f59
Daniel G <replayattack@pm.me>
2020-08-17 20:09:24 -0500

    Improve Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
   ...require("@snowpack/app-scripts-react/jest.config.js")(),
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.d.ts'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
