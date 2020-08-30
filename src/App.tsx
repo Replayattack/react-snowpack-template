7e27a321f779ddb3573c68ae00efe9f4785f9a1d
Daniel G <replayattack@pm.me>
2020-08-29 19:13:21 -0500

    Add React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 function App(): JSX.Element {
   return (
     <div className="min-h-screen flex items-center justify-center antialiased">
+      <Helmet>
+        <title>Template</title>
+      </Helmet>
       <h1 className="text-2xl">
         React template with Snowpack, TypeScript and Tailwind CSS
       </h1>
